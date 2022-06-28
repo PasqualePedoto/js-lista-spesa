9ce7ff6c8b36c9a5ed7eaff8409b2d9aa2ec5921
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-06-28 15:11:25 +0200

    complete excercise

--- js/script.js
+++ js/script.js
@@ -5,8 +5,31 @@
 const listItem = ['Uova', 'Pane', 'Latte', 'Biscotti', 'Pasta', 'Affettati', 'Formaggio'];
 
 // $ Bersagliamo un elemento nel DOM così da creare la lista della spesa
+const paragraph = document.getElementById('paragraph');
+
 // * Realizziamo un ciclo while
 
 //Dichiariamo e definiamo una variabile contatore per il while
+let i = 0;
 
-while (listItem.length)
+//Creiamo ed agganciamo una lista ordinata servendoci del createElement
+const orderedList = document.createElement('ul');
+paragraph.appendChild(orderedList);
+
+while (i < listItem.length) {
+    //Creiamo una lista ordinata servendoci del createElement
+    const element = document.createElement('li');
+
+    //Aggiungiamo l'elemento della spesa
+    element.innerText = listItem[i];
+
+    //Agganciamo il li all'ul
+    orderedList.appendChild(element);
+
+    //Incrementiamo la variabile contatore così da realizzare la logica del for
+    i++;
+}
+
+
+
+
