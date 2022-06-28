392380463ce85e71cb19b86a9d80418b75d7d268
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-06-28 14:42:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// # Realizziamo una stampa della lista della spesa
+
+// $ Creiamo un vettore con gli elementi della lista della spesa
+
+const listItem = ['Uova', 'Pane', 'Latte', 'Biscotti', 'Pasta', 'Affettati', 'Formaggio'];
+
+// $ Bersagliamo un elemento nel DOM così da creare la lista della spesa
+// * Realizziamo un ciclo while
+
+//Dichiariamo e definiamo una variabile contatore per il while
+
+while (listItem.length)
